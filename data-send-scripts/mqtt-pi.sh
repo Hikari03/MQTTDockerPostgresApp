929632c89c17db0f9fcd39b88e99ad8a0023ce66
David Houdek <david.houdek11@gmail.com>
2024-03-10 14:41:49 +0100

    mqttbroker confing and prepare pi script for authentication

--- data-send-scripts/mqtt-pi.sh
+++ data-send-scripts/mqtt-pi.sh
@@ -3,6 +3,6 @@
 while :
 do
 	TEMP="$(cat ~/FTP/share/www/assets/temperature.txt)"
-	mosquitto_pub -h mqttbroker.houdeda2.cz -t "temperature" -m "Trutnov:$TEMP"
+	mosquitto_pub -h mqttbroker.houdeda2.cz -t "temperature" -m "Trutnov:$TEMP" -u "temperature" -P "varilamysickakasicku"
 	sleep 300
 done
